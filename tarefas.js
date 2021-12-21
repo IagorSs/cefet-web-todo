eba26ec9ee59bd4674571fa84e47e9d69936013c
IagorSs <iagor.penaranda@uol.com.br>
2021-12-21 15:11:58 -0300

    Exercice 1 Done

--- tarefas.js
+++ tarefas.js
@@ -1,8 +1,21 @@
 class Tarefa {
+  #listaTarefas = document.getElementById('lista-tarefas');
+
   constructor(nome, categoria, realizada=false) {
     this.nome = nome;
     this.categoria = categoria;
     this.realizada = realizada;
+  }
+
+  adicionaNaPagina() {
+    const itemTarefa = document.createElement('li');
+    itemTarefa.classList.add('item-tarefa');
+    itemTarefa.classList.add(`categoria-${this.categoria}`);
+    itemTarefa.innerText = this.nome;
+
+    if(this.realizada) itemTarefa.classList.add('marcado');
+
+    this.#listaTarefas.appendChild(itemTarefa);
   }
 }
 
@@ -10,3 +23,8 @@ const tarefas = [
   new Tarefa('Comprar leite', 'compras'),
   new Tarefa('Escutar chimbinha', 'lazer', true)
 ];
+
+const listaTarefas = document.getElementById('lista-tarefas');
+listaTarefas.innerHTML = "";
+
+tarefas.forEach(item => item.adicionaNaPagina());
